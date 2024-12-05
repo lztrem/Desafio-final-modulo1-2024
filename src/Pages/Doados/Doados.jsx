2a9e3163e9ae9e3c08983d640010f895fbab732a
lztrem <lzofficial10@gmail.com>
2024-12-04 21:39:12 -0300

    Projeto final finalizado

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,10 +1,20 @@
+import S from "./doados.module.scss"
+import livrored from "../../assets/livrodoado.png"
 
 
 
 export default function Doados(){
     return(
-        <section>
-            <h2>Doados</h2>
+        <section className={S.boxDoados}>
+            <h2>Doados Livros</h2>
+            <section className={S.boxcard}>
+                <article>
+                    <img src={livrored} alt="" />
+                    <h3>O Protagonista</h3>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
+                </article>
+            </section>
         </section>
     )
 }
